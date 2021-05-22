8b94cf3bc475eb0e5ab7344018f877482a400ce7
gtg7784 <tae.gun7784@gmail.com>
2021-05-23 04:27:37 +0900

    chore: update api baseurl

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,5 +4,5 @@ axios.defaults.timeout = 10000;
 axios.defaults.headers.common.Accept = '*/*';
 
 export default axios.create({
-  baseURL: 'https://merging-point.transign.co/api',
+  baseURL: 'https://api-merging-point.transign.co/api',
 });
