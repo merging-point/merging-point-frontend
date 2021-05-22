47873ca401af12b3ab5c33744f490e8d4a0252f8
GangbacOl <gangyeonjo336@gmail.com>
2021-05-23 03:15:04 +0900

    chore: update server baseurl

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,4 +3,6 @@ import axios from 'axios';
 axios.defaults.timeout = 10000;
 axios.defaults.headers.common.Accept = '*/*';
 
-export default axios.create({ baseURL: 'https://e030e1b57e01.ngrok.io/api' });
+export default axios.create({
+  baseURL: 'https://merging-point.transign.co/api',
+});
