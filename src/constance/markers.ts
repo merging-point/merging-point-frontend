1594d8edf29881d6f3060eb4a53b1c6e0c388da3
Lee Seungmin <lee@seungmin.io>
2021-05-22 23:03:19 +0900

    feat: kakao map MarkerImage array

---
+++ src/constance/markers.ts
@@ -0,0 +1,14 @@
+/* eslint-disable import/prefer-default-export */
+
+const { kakao } = window;
+
+export default new Array(100)
+  .fill(null)
+  .map(
+    (_, index) =>
+      new kakao.maps.MarkerImage(
+        require(`../assets/markers/${index + 1}.png`),
+        new kakao.maps.Size(50, 50),
+        { offset: new kakao.maps.Point(15, 15) },
+      ),
+  );
