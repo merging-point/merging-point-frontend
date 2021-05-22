f9c8a5b3f06c63f8fbe9ff9b86cf607fe6338beb
Lee Seungmin <lee@seungmin.io>
2021-05-22 16:20:51 +0900

    feat: Main page

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { Route, Switch } from "react-router";
-import { BrowserRouter } from "react-router-dom";
-import { Root } from "../pages";
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import { Main, Root } from '../pages';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Root} />
+      <Route exact path="/main" component={Main} />
     </Switch>
   </BrowserRouter>
 );
